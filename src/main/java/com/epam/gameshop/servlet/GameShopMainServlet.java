9599afc2da7769bbf58f648e1ff8f9c6daa7e134
KorneenkovAnton <41143644+KorneenkovAnton@users.noreply.github.com>
2021-06-27 12:55:18 +0600

    refactore: rename MainServlet

--- src/main/java/com/epam/gameshop/servlet/GameShopMainServlet.java
+++ src/main/java/com/epam/gameshop/servlet/GameShopMainServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MainServlet extends HttpServlet implements Constants {
+public class GameShopMainServlet extends HttpServlet implements Constants {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
